39ec81db262ba3000f7666c4b00a60c59dd5461f
ClaudiaShu <claudia.shu@outlook.com>
2025-05-20 12:02:36 +0100

    update

--- sonnet/data_config.py
+++ sonnet/data_config.py
@@ -1,12 +1,10 @@
 from dataclasses import dataclass
 from sonnet.data.dartsDataloader import CustomDartsDataModule
-from sonnet.data.iliDataloader import CustomILIDataModule
 from sonnet.data.weatherDataloader import CustomWeatherDataModule
 
 # Map dataset names to their corresponding DataModule classes
 data_mapping = {
     "darts": CustomDartsDataModule,
-    "ili_trends": CustomILIDataModule,
     "weatherbench": CustomWeatherDataModule,
 }
 
